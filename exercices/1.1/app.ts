aaf6e14c3aca0490eff3f357fc132c945bdcd467
asma-mellah-vinci <asma.mellah@student.vinci.be>
2025-09-20 17:19:13 +0200

    new: ex1.1

---
+++ exercices/1.1/app.ts
@@ -0,0 +1,15 @@
+import express, { ErrorRequestHandler } from "express";
+import filmsRouter from "./routes/films";
+
+const app = express();
+app.use(express.json());
+
+app.use("/films", filmsRouter);
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.error(err.stack);
+  return res.status(500).send("Something broke!");
+};
+app.use(errorHandler);
+
+export default app;
