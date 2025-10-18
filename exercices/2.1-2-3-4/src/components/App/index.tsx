9cb60e0ce98546e7ad6a290f99fd93dfab3c039c
asma-mellah-vinci <asma.mellah@student.vinci.be>
2025-10-18 19:55:22 +0200

    new:ex2.4

--- exercices/2.1-2-3-4/src/components/App/index.tsx
+++ exercices/2.1-2-3-4/src/components/App/index.tsx
@@ -1,5 +1,7 @@
 import PageTitle from "../PageTitle";
 import Cinema from "../Cinema";
+import Header from "../Header";
+import Footer from "../Footer";
 
 
 const App = () => {
@@ -47,14 +49,27 @@ const moviesCinema2 = [
   },
 ]; 
 
+  const logoUrl = "https://images.unsplash.com/photo-1760662418182-932278aab280?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=987";
+  const logoUrlFooter = "https://plus.unsplash.com/premium_photo-1664303124313-126bf7456982?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1073";
+
   return (
     <div>
+
+      <Header logo={logoUrl}>
+        <h1>Bienvenue sur CineWorld</h1>
+      </Header>
+
       <PageTitle title={pageTitle} />
 
       <Cinema name={cinema1Name} movies= {moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+
+       <Footer logo={logoUrlFooter}>
+        <h5>Ceci est la fin de mon site</h5>
+      </Footer>
     </div>
+    
   );
 };
 
