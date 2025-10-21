ea037d8e650ad753837bab215422a8d6c3b31b54
asma-mellah-vinci <asma.mellah@student.vinci.be>
2025-10-21 12:07:40 +0200

    new:ex2.5-destructuring

---
+++ exercices/2.5/src/App.tsx
@@ -0,0 +1,11 @@
+import ClickCounter from "./components/ClickCounter";
+
+const App = () => {
+  return (
+    <div>
+      <ClickCounter />
+    </div>
+  );
+};
+
+export default App;
---
+++ exercices/2.5/src/components/ClickCounter/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const ClickCounter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+     <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src\components\ClickCounter\index.tsx</code> and save to test HMR
+        </p>
+      </div>
+  );
+};
+
+export default ClickCounter;
+
