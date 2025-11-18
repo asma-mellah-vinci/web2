850307bf007bc55c7365c6c9ffa92cbe2df02186
asma-mellah-vinci <asma.mellah@student.vinci.be>
2025-11-18 22:03:51 +0100

    new:ex2.14b-refactor

---
+++ exercices/2.14b/src/component/RandomDog.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./RandomDog.css";
+
+
+interface dog {
+  message : string;
+  status : string;
+}
+
+
+const RandomDog = () => {
+    const [dog , setDog] = useState<dog | undefined>(undefined);
+
+
+
+    useEffect(() => {
+        fetchRandomDog();
+    } , []);
+
+    const fetchRandomDog = async () => {
+
+        try {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+            if(!response.ok){
+                throw new Error("fetch error");
+            }
+
+            const randomDog = await response.json();
+            setDog({
+                message : randomDog.message,
+                status : randomDog.status,
+            });
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    if(!dog){
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <img 
+        className="random-dog"
+        src={dog.message} 
+        alt="random dog"
+         />
+    );
+}
+
+export default RandomDog;
